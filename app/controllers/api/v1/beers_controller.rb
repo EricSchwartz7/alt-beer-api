df0fa80279fcd56c4cc992009f729866371108f5
Clinton Nguyen <clinton.ngyn@gmail.com>
2017-02-03 16:29:57 -0500

    DONEEEEE

--- app/controllers/api/v1/beers_controller.rb
+++ app/controllers/api/v1/beers_controller.rb
@@ -13,11 +13,24 @@ module Api::V1
     end
 
     def alcohol
-      binding.pry
       @beers = Beer.all.where('alcohol <= ?', (params[:upper].to_f / 10).to_s)
       render json: @beers
+      Beer.all.where('alcohol <= 2.0')
     end
 
+    def create
+      @beer = Beer.new(beer_params)
+      @beer.alcohol /= 10
+      @beer.save
+      @style = Style.find_or_create_by(name: params[:style])
+      @style << @beer
+    end
+
+    private
+
+    def beer_params
+      params.permit(:name, :ibu, :alcohol)
+    end
 
   end
 
